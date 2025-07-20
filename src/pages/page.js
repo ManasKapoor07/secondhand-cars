c9a2aef42438281925a72ea625558d7681afe99a
mkapoor-datopic <mkapoor@datopic.com>
2025-07-20 19:48:00 +0530

    page

---
+++ src/pages/page.js
@@ -0,0 +1,14 @@
+// pages/page.tsx
+
+import React from "react";
+
+const HomeMain = () => {
+  return (
+    <div className="p-6 text-lg">
+      {/* Your homepage content */}
+      Welcome to Maa Bahawani Car Bazar
+    </div>
+  );
+};
+
+export default HomeMain;
